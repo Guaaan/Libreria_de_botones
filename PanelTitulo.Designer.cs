f657dee8f8b34902f08a0acb38486edad301a49c
guaaan <juampablo.txt@gmail.com>
2021-11-03 14:26:43 -0300

    enviar

---
+++ PanelTitulo.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace btnLib
+{
+    partial class PanelTitulo
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlFondo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pnlFondo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlFondo
+            // 
+            this.pnlFondo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(76)))), ((int)(((byte)(155)))));
+            this.pnlFondo.Controls.Add(this.lblTitulo);
+            this.pnlFondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlFondo.Location = new System.Drawing.Point(0, 0);
+            this.pnlFondo.Name = "pnlFondo";
+            this.pnlFondo.Size = new System.Drawing.Size(460, 50);
+            this.pnlFondo.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitulo.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(460, 50);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Texto";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PanelTitulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlFondo);
+            this.Name = "PanelTitulo";
+            this.Size = new System.Drawing.Size(460, 50);
+            this.pnlFondo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlFondo;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ PanelTitulo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace btnLib
+{
+    public partial class PanelTitulo : UserControl
+    {
+
+        private Color cFondo = Color.FromArgb(0, 76, 155);
+        private String text = "Botón";
+
+        [Category("Editores")]
+        public String Titulo
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                lblTitulo.Text = text;
+            }
+        }
+
+
+
+        [Category("Editores")]
+        public Color Color1_Arriba
+        {
+            get
+            {
+                return cFondo;
+            }
+            set
+            {
+                cFondo = value;
+                pnlFondo.BackColor = cFondo;
+            }
+        }
+
+        public PanelTitulo()
+        {
+            InitializeComponent();
+        }
+    }
+}
