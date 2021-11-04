db0679a6415561261c8d240b9208f770c59e500c
guaaan <juampablo.txt@gmail.com>
2021-11-04 09:36:25 -0300

    enviar

---
+++ btnNaranjaBrillante.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace btnLib
+{
+    partial class btnNaranjaBrillante
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonNaranjaBrillante = new Guna.UI.WinForms.GunaGradientButton();
+            this.SuspendLayout();
+            // 
+            // botonNaranjaBrillante
+            // 
+            this.botonNaranjaBrillante.Animated = true;
+            this.botonNaranjaBrillante.AnimationHoverSpeed = 0.07F;
+            this.botonNaranjaBrillante.AnimationSpeed = 0.03F;
+            this.botonNaranjaBrillante.BackColor = System.Drawing.Color.Transparent;
+            this.botonNaranjaBrillante.BaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(130)))), ((int)(((byte)(67)))));
+            this.botonNaranjaBrillante.BaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(88)))), ((int)(((byte)(32)))));
+            this.botonNaranjaBrillante.BorderColor = System.Drawing.Color.Black;
+            this.botonNaranjaBrillante.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.botonNaranjaBrillante.FocusedColor = System.Drawing.Color.Empty;
+            this.botonNaranjaBrillante.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonNaranjaBrillante.ForeColor = System.Drawing.Color.White;
+            this.botonNaranjaBrillante.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.botonNaranjaBrillante.Image = null;
+            this.botonNaranjaBrillante.ImageSize = new System.Drawing.Size(20, 20);
+            this.botonNaranjaBrillante.Location = new System.Drawing.Point(0, 0);
+            this.botonNaranjaBrillante.Name = "botonNaranjaBrillante";
+            this.botonNaranjaBrillante.OnHoverBaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(77)))), ((int)(((byte)(55)))));
+            this.botonNaranjaBrillante.OnHoverBaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(77)))), ((int)(((byte)(55)))));
+            this.botonNaranjaBrillante.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.botonNaranjaBrillante.OnHoverForeColor = System.Drawing.Color.White;
+            this.botonNaranjaBrillante.OnHoverImage = null;
+            this.botonNaranjaBrillante.OnPressedColor = System.Drawing.Color.Black;
+            this.botonNaranjaBrillante.OnPressedDepth = 50;
+            this.botonNaranjaBrillante.Radius = 8;
+            this.botonNaranjaBrillante.Size = new System.Drawing.Size(98, 32);
+            this.botonNaranjaBrillante.TabIndex = 5;
+            this.botonNaranjaBrillante.Text = "Botón";
+            this.botonNaranjaBrillante.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.botonNaranjaBrillante.UseTransfarantBackground = true;
+            // 
+            // btnNaranjaBrillante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Controls.Add(this.botonNaranjaBrillante);
+            this.Name = "btnNaranjaBrillante";
+            this.Size = new System.Drawing.Size(98, 32);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaGradientButton botonNaranjaBrillante;
+    }
+}
---
+++ btnNaranjaBrillante.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace btnLib
+{
+    public partial class btnNaranjaBrillante : UserControl
+    {
+        private Color arribColor = Color.FromArgb(255, 130, 67);
+        private Color abajColor = Color.FromArgb(199, 88, 32);
+        private Color arribHover = Color.FromArgb(132, 58, 21);
+        private Color abajHover = Color.FromArgb(132, 58, 21);
+
+        private Size tamaño = new Size(130, 40);
+
+        private Image img;
+        private Size imgSize = new Size(25, 25);
+
+        private String text = "Botón";
+        private int bRadius = 8;
+
+        private Font fontD = new Font("Tahoma", 10, FontStyle.Bold, GraphicsUnit.Point);
+
+
+        [Category("Editores")]
+        public Font Fuente
+        {
+            get { return fontD; }
+            set
+            {
+                fontD = value;
+                botonNaranjaBrillante.Font = fontD;
+            }
+        }
+
+
+        [Category("Editores")]
+        public Color Color1_Arriba
+        {
+            get
+            {
+                return arribColor;
+            }
+            set
+            {
+                arribColor = value;
+                botonNaranjaBrillante.BaseColor1 = arribColor;
+            }
+        }
+        [Category("Editores")]
+        public Color Color2_Abajo
+        {
+            get
+            {
+                return abajColor;
+            }
+            set
+            {
+                abajColor = value;
+                botonNaranjaBrillante.BaseColor2 = abajColor;
+
+            }
+        }
+
+        [Category("Editores")]
+        public Color Hover1_Arriba
+        {
+            get
+            {
+                return arribHover;
+            }
+            set
+            {
+                arribHover = value;
+                botonNaranjaBrillante.OnHoverBaseColor1 = arribHover;
+            }
+        }
+        [Category("Editores")]
+        public Color Hover2_Abajo
+        {
+            get
+            {
+                return abajHover;
+            }
+            set
+            {
+                abajHover = value;
+                botonNaranjaBrillante.OnHoverBaseColor2 = abajHover;
+            }
+        }
+        [Category("Editores")]
+        public Size Tamaño_Boton
+        {
+            get
+            {
+                return tamaño;
+            }
+
+            set
+            {
+                tamaño = value;
+                botonNaranjaBrillante.Size = tamaño;
+                this.Size = tamaño;
+            }
+        }
+        [Category("Editores")]
+        public String Texto
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+                botonNaranjaBrillante.Text = text = text.Replace("@", "" + "\n");
+            }
+        }
+
+        public enum TextoAlineo
+        {
+            Center,
+            Left,
+            Right
+        }
+        TextoAlineo textoAlineo;
+
+        [Category("Editores")]
+        public TextoAlineo Alineacion_texto
+        {
+            get { return textoAlineo; }
+            set
+            {
+                textoAlineo = value; Invalidate();
+                switch (textoAlineo)
+                {
+                    case TextoAlineo.Center:
+                        botonNaranjaBrillante.TextAlign = HorizontalAlignment.Center;
+                        break;
+
+                    case TextoAlineo.Right:
+                        botonNaranjaBrillante.TextAlign = HorizontalAlignment.Right;
+                        break;
+
+                    case TextoAlineo.Left:
+                        botonNaranjaBrillante.TextAlign = HorizontalAlignment.Left;
+                        break;
+
+                }
+            }
+        }
+
+        [Category("Editores")]
+        public Image Imagen
+        {
+            get
+            {
+                return img;
+            }
+            set
+            {
+                img = value;
+                botonNaranjaBrillante.Image = img;
+            }
+        }
+        [Category("Editores")]
+        public Size Tamaño_imagen
+        {
+            get
+            {
+                return imgSize;
+            }
+            set
+            {
+                imgSize = value;
+                botonNaranjaBrillante.ImageSize = imgSize;
+            }
+        }
+        [Category("Editores")]
+        public int Radio
+        {
+            get { return bRadius; }
+            set
+            {
+                bRadius = value;
+                botonNaranjaBrillante.Radius = bRadius;
+            }
+
+        }
+        public btnNaranjaBrillante()
+        {
+            InitializeComponent();
+        }
+    }
+}
